3d6f754d6f0faab6c00e9ca33c4dc52a5b958d7d
skyleaworlder <870033938@qq.com>
2020-07-16 20:01:11 +0800

    A/R: add gitignore, rename gcd -> calcu

--- Calcu.py
+++ Calcu.py
@@ -1,4 +1,14 @@
 import sys
+from math import ceil
+
+def Add(a, b, n):
+    return (a + b) % n
+
+def Sub(a, b, n):
+    return (a - b) % n
+
+def Mul(a, b, n):
+    return (a * b) % n
 
 def GCD(a, b):
     return a if b == 0 else GCD(b, a % b)
@@ -28,12 +38,21 @@ def Inverse(a, n):
             t_arr.append((-1)**(i+1)*(abs(t_arr[i]) + q_arr[i+1] * abs(t_arr[i+1])))
         return t_arr[-1] % n # return the last elem in t_arr
 
+def Div(a, b, p):
+    return Mul(a, Inverse(b, p), p)
+
+def Sqrt(z, n):
+    for i in range(0, ceil((n+1) / 2)):
+        if pow(i, 2, n) == z:
+            return [i, n - i]
+    else:
+        return [-1, -1]
+
 def main(argv):
     if argv[0] == "--gcd" or argv[0] == '-g':
         print("gcd("+argv[1]+","+argv[2]+") = ", GCD(int(argv[1]), int(argv[2])))
     if argv[0] == "--rev" or argv[0] == '-r':
         print(argv[1]+"^{-1} mod "+argv[2]+" = ", Inverse(int(argv[1]), int(argv[2])))
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
