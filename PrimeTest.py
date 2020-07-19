4712db619c376a9f9ac02784e8b1806145a23ce4
skyleaworlder <870033938@qq.com>
2020-07-19 22:45:07 +0800

    M: add Miller-Rabin and Trial prime test

--- PrimeTest.py
+++ PrimeTest.py
@@ -1,21 +1,24 @@
 '''
-python PrimeTest.py [<test-num>] [<iter-num>]
-eg. python PrimeTest.py 64 12
+python PrimeTest.py [<choice>] [<test-num>] [<iter-num>]
+eg. python PrimeTest.py -m/--Miller-Rabin 64 12
+    python PrimeTest.py -f/--Fermat 65 12
+    python PrimeTest.py -t/--Trial 128
 '''
 import sys
 import random
 from Calcu import GCD
+from ConMod import DIVNum
+from sympy import prime, nextprime
+from math import ceil, sqrt
 
 '''
 n is input to be tested
 iterNum
 '''
-def Fermat(n_i, iterNum_i=512):
-    n = int(n_i)
+def Fermat(n, iterNum=512):
     if n == 2 or n == 3:
         return True
 
-    iterNum = int(iterNum_i)
     for i in range(0, iterNum):
         a = random.randint(2, n-2)
         if GCD(a, n) != 1:
@@ -25,11 +28,51 @@ def Fermat(n_i, iterNum_i=512):
     else:
         return True
 
+def Trial(n):
+    p = prime(1)
+    bound = ceil(sqrt(n))
+    while p < bound:
+        if n % p == 0:
+            return False
+        p = nextprime(p)
+    return True
+
+def MillerRabin(n, iterNum=512):
+    r = DIVNum(2, n-1)
+    d = (n-1) // pow(2,r)
+
+    cnt = 0
+    while cnt < iterNum:
+        a = random.randint(2, n)
+        if pow(a, d, n) == 1 or pow(a, d, n) == n-1:
+            return True
+
+        r_0 = 0
+        while r_0 < r:
+            if pow(a, pow(2,r_0)*d, n) == n-1:
+                return True
+            r_0 += 1
+        cnt += 1
+
+    return False
+
 def main(argv):
-    if Fermat(argv[0], argv[1]):
-        print(argv[0], "is a prime perhaps.")
+    if argv[0] == "-m" or argv[0] == "--Miller-Rabin":
+        flag = MillerRabin(int(argv[1]), int(argv[2]))
+        print("using Miller-Rabin prime test:")
+    elif argv[0] == "-f" or argv[0] == "--Fermat":
+        print("using Fermat prime test:")
+        flag = Fermat(int(argv[1]), int(argv[2]))
+    elif argv[0] == "-t" or argv[0] == "--Trial":
+        print("using Trial test:")
+        flag = Trial(int(argv[1]))
     else:
-        print(argv[0], "shouldn't be a prime.")
+        pass
+
+    if flag:
+        print(argv[1], "is a prime perhaps.")
+    else:
+        print(argv[1], "shouldn't be a prime.")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
