8a4df8c9fd7e58eb09259b9cd6f867c1c8dc26ca
skyleaworlder <870033938@qq.com>
2020-07-21 01:33:11 +0800

    M: add a func which can solve Simple Congruence Equation

--- ConMod.py
+++ ConMod.py
@@ -1,4 +1,5 @@
 import sys
+from Calcu import GCD, Inverse, Mul
 
 def CMVerify(a, b, n):
     return (a%n == b%n)
@@ -12,3 +13,17 @@ def DIVNum(a, b):
         b = b // a
         cnt += 1
     return cnt
+
+def CongEq(a, b, n):
+    d = GCD(a, n)
+    if d != 1 and not DIVVerify(d, b):
+        return [-1]
+    elif d != 1:
+        res =  CongEq(a//d, b//d, n//d)
+        k = 1
+        while k*n//d < n:
+            res.append(res[0] + k*n//d)
+            k += 1
+        return res
+    else:
+        return [Mul(b, Inverse(a, n), n)]
