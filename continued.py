908e8d3d93209007a68d0b7c7f7e87acbb1ff17e
skyleaworlder <870033938@qq.com>
2020-12-24 10:39:33 +0800

    M: add layer assert & layer restriction in continued.py

--- continued.py
+++ continued.py
@@ -1,31 +1,35 @@
 '''
-python continued.py [<nume>] [<deno>]
-eg. python continued.py 17 45
+python continued.py [<nume>] [<deno>] [<expand-layer>]
+eg. python continued.py 17 45 4
 '''
 import sys
 from Calcu import GCD
 
 class continuedFrac:
 
-    def __init__(self, nume, deno):
+    def __init__(self, nume, deno, layer):
         assert GCD(nume, deno) == 1
         self.nume = nume
         self.deno = deno
+        self.layer = layer
+        assert layer >= 2
         self.a_arr = []
         self.p_arr = []
         self.q_arr = []
         self.a_gen()
         self.pq_gen()
 
-    def _a_calcu(self, nume, deno, arr):
-        if deno == 1:
+    def _a_calcu(self, nume, deno, arr, layer):
+        if layer > self.layer:
+            pass
+        elif deno == 1:
             arr.append(nume)
         else:
             arr.append(nume // deno)
-            self._a_calcu(deno, nume % deno, arr)
+            self._a_calcu(deno, nume % deno, arr, layer+1)
 
     def a_gen(self):
-        self._a_calcu(self.nume, self.deno, self.a_arr)
+        self._a_calcu(self.nume, self.deno, self.a_arr, 1)
 
     def pq_gen(self):
         '''
@@ -44,8 +48,16 @@ class continuedFrac:
 
 
 def main(argv):
-    obj = continuedFrac(int(argv[0]), int(argv[1]))
-    print(argv[0]+"/"+argv[1]+"'s abbreviated notation is", obj.a_arr)
+    nume, deno = int(argv[0]), int(argv[1])
+    '''
+    because wienner-attack, add member:layer
+    but use continued.py always needn't param layer
+    if python continued.py 17 45 => layer = 2
+    else python continued.py 17 45 4 => layer = 4
+    '''
+    layer = 2 if len(argv) <= 2 else int(argv[2])
+    obj = continuedFrac(nume, deno, layer)
+    print(argv[0]+"/"+argv[1]+"'s abbreviated notation is", obj.a_arr, "(layer = "+str(layer)+")")
     print("and nume arr is", obj.p_arr)
     print("    deno arr is", obj.q_arr)
 
