f9193bedff3dbc25968b433fe157e3171bb7ce97
skyleaworlder <870033938@qq.com>
2021-01-19 18:27:29 +0800

    M: delete QuadSieve in IntFactorize

--- IntFactorize.py
+++ IntFactorize.py
@@ -25,103 +25,8 @@ from PrimeTest import Fermat
 from ConMod import CMVerify, DIVVerify, DIVNum
 from Calcu import GCD
 from math import sqrt, ceil
-import numpy as np
 from functools import reduce
 from continued import continuedFrac
-
-'''
-draft of quadratic sieve.
-cannot work and perform well for several reasons.
-'''
-solve = []
-class QuadSieve:
-
-    def __init__(self, n):
-        self.n = n
-        self.p = []
-        self.q = []
-
-    def _Y_calcu(self, x, n):
-        return (x + ceil(sqrt(n)))**2 - n
-
-    def _DIV_total(self, base, v):
-        ret = v
-        cnt = 0
-        while DIVVerify(base, ret):
-            ret = ret // base
-            cnt += 1
-        return {
-            "ret": ret,
-            "cnt": cnt
-        }
-
-    def _solve_backTrack(self, coef, ans, layer):
-        '''
-        back track method
-        to find a vector can solve cong_equ
-        '''
-        global solve
-        if layer == ans.shape[1]:
-            return
-        for i in range(1, -1, -1):
-            ans[0][layer] = i
-            if (np.mod(np.dot(ans, coef), 2) == np.zeros([ans.shape[0], coef.shape[1]], dtype=int)).all() \
-                and (np.array(ans, dtype=int) != np.zeros(ans.shape, dtype=int)).any():
-                solve.append(ans[0].tolist())
-                #print(ans[0].tolist(), solve)
-            #print(layer, "?", ans)
-            self._solve_backTrack(coef, ans, layer+1)
-
-    def _solve(self, coef):
-        ans = np.zeros([1, coef.shape[0]], dtype=int)
-        self._solve_backTrack(coef, ans, layer=0)
-        return solve
-
-    def QuadSieve(self, baseNum):
-        '''
-        V:      vector of Y(x)
-        B:      prime bases arr
-        Coef:   a matrix(B.len*B.len) of decomped result,
-                the same as Coef Matrix in Dixon ALG
-        Y:      res
-        '''
-        global solve
-        assert not Fermat(self.n)
-        self.V = [self._Y_calcu(x, self.n) for x in range(0, 100)]
-        V_tmp = self.V
-        self.B = [x for x in range(2, baseNum) if legendre(self.n, x) == 1 and Fermat(x)]
-
-        for base in self.B:
-            V_tmp = [self._DIV_total(base, v)["ret"] for v in V_tmp]
-
-        Y_index_arr = [index for index, v in enumerate(V_tmp) if v == 1]
-        self.Y = [self.V[index] for index in Y_index_arr]
-
-        ''' div y by base^k if base^k | y '''
-        self.Coef = np.zeros([self.Y.__len__(), self.B.__len__()], dtype=int)
-        for jndex,y in enumerate(self.Y):
-            for index,base in enumerate(self.B):
-                self.Coef[jndex][index] = self._DIV_total(base, y)["cnt"]
-        self.Coef = np.mod(self.Coef, 2)
-
-        ''' del all 0 rows '''
-        res_row = [index for index,val in enumerate(self.Coef.any(axis=1)) if val]
-        Y_index_arr = [val for index,val in enumerate(Y_index_arr) if index in res_row]
-        self.Coef = self.Coef[res_row, :]
-
-        ''' solve cong_equ(mod 2) '''
-        self.R = self._solve(self.Coef)
-        #print(self.V, '\n', V_tmp, self.R, solve, Y_index_arr, "???")
-
-        ''' resolve and calcu p_arr, q_arr '''
-        for r in self.R:
-            Y_index_arr_turn = [val for index,val in enumerate(Y_index_arr) if r[index] != 0]
-            square = reduce(lambda x,y : x*self.V[y], Y_index_arr_turn, 1)
-            V_2 = [self._Y_calcu(x, self.n)+self.n for x in Y_index_arr_turn]
-            square2 = reduce(lambda x,y : x*y, V_2, 1)
-            self.p.append(GCD(sqrt(square) + sqrt(square2), self.n))
-            self.q.append(GCD(abs(sqrt(square2) - sqrt(square)), self.n))
-        print(self.p, '\n', self.q)
 
 '''
 Pollard-Rho ALG to decompose a composite number.
@@ -262,12 +167,7 @@ def WienerAttack(n, b):
 
 def main(argv):
     choice = argv[0]
-    if choice == "--QuadSieve" or choice == "-qs":
-        n = int(argv[1])
-        baseNum = int(argv[2])
-        qs = QuadSieve(n)
-        qs.QuadSieve(baseNum)
-    elif choice == "--Rho" or choice == "-r":
+    if choice == "--Rho" or choice == "-r":
         mode = argv[1]
         n = int(argv[2])
         rho = PollardRho(n)
