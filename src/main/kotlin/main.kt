9c8b5192c730f018d0e74efdccde9a560d9543ee
skyleaworlder <870033938@qq.com>
2021-01-23 00:28:12 +0800

    A: add idea-kt-init

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,16 @@
+import org.python.util.PythonInterpreter
+import org.python.core.*
+import java.util.*
+
+fun main() {
+    // for test, demonstrate it.
+    val props = Properties()
+    props.setProperty("D:\\python\\Python27\\python.exe", "D:\\Projects\\Gitexercise\\ShiCrypto")
+    PythonInterpreter.initialize(System.getProperties(), props, arrayOf(".py","--add", "4", "5", "9"))
+    val interpreter = PythonInterpreter()
+    interpreter.exec("import sys")
+    interpreter.exec("sys.path.append('D:\\Projects\\Gitexercise\\ShiCrypto\\plugin\\\\api')")
+    interpreter.exec("print sys.path")
+    interpreter.exec("print sys.argv")
+    interpreter.execfile("D:\\Projects\\Gitexercise\\ShiCrypto\\plugin\\api\\Calcu.py")
+}
